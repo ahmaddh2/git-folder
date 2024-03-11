7a197d394b3f88ccf797bf4f0856434f9788f23e
unknown <you@example.com>
2024-03-11 22:59:59 +0200

    5st_commit

--- e.js
+++ e.js
@@ -1,4 +1,8 @@
-function test(){ 
-    alert('full stack trainng!!!!!');
+const express = require('express')
+const app = express()
 
-}
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
